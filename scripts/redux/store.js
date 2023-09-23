44844bd79410470bd6ff6241228693af91f88367
Sergey Salomaha <s.salomaha@gmail.com>
2023-09-23 20:36:10 +0300

    Lesson 2.7 Replace own redux with redux package

--- scripts/redux/store.js
+++ scripts/redux/store.js
@@ -1,32 +1,8 @@
+import { createStore, combineReducers } from '../../node_modules/redux/es/redux.mjs'
 import { productsReducer } from './productsReducer.js'
 
-class Store {
-  state = {}
-  subscribers = []
-
-  constructor(reducers) {
-    this.reducers = reducers
-  }
-
-  getState() {
-    return this.state
-  }
-
-  dispatch(action) {
-    for (const reducerName in this.reducers) {
-      const reducer = this.reducers[reducerName]
-      const newState = reducer(this.state[reducerName], action)
-      this.state[reducerName] = newState
-    }
-
-    this.subscribers.forEach(cb => cb())
-  }
-
-  subscribe(cb) {
-    this.subscribers.push(cb)
-  }
-}
-
-export const store = new Store({
-  products: productsReducer
-})
+export const store = createStore(
+  combineReducers({
+    products: productsReducer
+  })
+)
