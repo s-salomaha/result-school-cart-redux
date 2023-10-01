c17fd6cf65a73594e2f0231774cccd714d985603
Sergey Salomaha <s.salomaha@gmail.com>
2023-10-01 20:04:18 +0300

    Lesson 4.6 Save state of store on page reload

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,27 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { productsReducer } from './productsReducer'
-import {logActionMiddleware} from "./logActionMiddleware";
+import { logActionMiddleware } from "./logActionMiddleware";
 import { orderReducer } from "./orderReducer";
 import thunkMiddleware from "redux-thunk"
+import { persistReducer, persistStore } from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
-
-const rootReducer = combineReducers({
-  products: productsReducer,
-  order: orderReducer
-})
+const rootReducer = persistReducer(
+  { key: 'redux', storage: storage, throttle: 100000 },
+  combineReducers({
+    products: productsReducer,
+    order: orderReducer
+  })
+)
 
 export const store = createStore(
   rootReducer,
   applyMiddleware(thunkMiddleware, logActionMiddleware)
 )
+
+export const persistor = persistStore(store)
+
+// @ts-ignore
+window.persistor = persistor
 
 export type RootState = ReturnType<typeof rootReducer>
